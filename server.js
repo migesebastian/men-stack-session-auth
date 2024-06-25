9562b1b56c63a14aad537e66a3c20f80a30ab4db
mmarrone97 <mmarrone97@gmail.com>
2024-06-25 09:56:30 -0400

    create auth controller

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const morgan = require("morgan");
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
+// Auth Controller
+const authController = require("./controllers/auth.js");
+
+
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
@@ -29,6 +33,7 @@ app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
+app.use("/auth", authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
